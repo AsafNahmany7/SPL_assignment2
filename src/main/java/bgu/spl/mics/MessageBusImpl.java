bd73979cc5fba80d57fd171b8beb91223bcf5667
AsafNahmany7 <nahmany10012.net@gmail.com>
2024-12-29 11:21:38 +0200

    Initial commit

---
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -0,0 +1,165 @@
+package bgu.spl.mics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
+/**
+ * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
+ * Write your implementation here!
+ * Only one public method (in addition to getters which can be public solely for unit testing) may be added to this class
+ * All other methods and members you add the class must be private.
+ */
+public class MessageBusImpl implements MessageBus {
+
+	private static MessageBusImpl MessageBus = new MessageBusImpl();
+	private ConcurrentHashMap<MicroService, BlockingQueue<Message>> ServicesMessageQueues = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<Class<?extends Message>,BlockingQueue<MicroService>> BrodcastSubscribersQueues = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<Class<? extends Message>,BlockingQueue<MicroService>> EventsSubscribersQueues = new ConcurrentHashMap<>();
+	private ConcurrentHashMap<Event<?>,Future<?>> EventsFutures = new ConcurrentHashMap<>();
+
+
+	private MessageBusImpl() {}
+
+	// שיטה סטטית להחזרת המופע היחיד
+	public static MessageBus getInstance() {
+		return MessageBus;
+	}
+
+	@Override
+	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
+		EventsSubscribersQueues.putIfAbsent(type,new LinkedBlockingQueue<>());
+
+		if (!EventsSubscribersQueues.get(type).contains(m)) {
+			EventsSubscribersQueues.get(type).add(m);
+		}
+	}
+
+	@Override
+	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
+		BrodcastSubscribersQueues.putIfAbsent(type,new LinkedBlockingQueue<>());
+
+		if(!BrodcastSubscribersQueues.get(type).contains(m)) {
+			BrodcastSubscribersQueues.get(type).add(m);
+		}
+	}
+
+	@Override
+	public <T> void complete(Event<T> e, T result) {
+		if(!EventsFutures.containsKey(e))
+			throw new IllegalStateException("Event not found in EventsFutures: " + e);
+
+		Future<T> eventFu = (Future<T>)EventsFutures.get(e);
+		eventFu.resolve(result);
+	}
+
+	@Override
+	public void sendBroadcast(Broadcast b) {
+
+		if(!BrodcastSubscribersQueues.containsKey(b.getClass())) {
+			System.out.println("No Such brodcast is listed please list it with a subscribing microservice first!");
+			return;
+		}
+
+		if(BrodcastSubscribersQueues.get(b.getClass()).isEmpty()) {
+			System.out.println("No microservices subscribed with this event!");
+			return;
+		}
+
+		BlockingQueue<MicroService> microServicesQueue = BrodcastSubscribersQueues.get(b.getClass());
+
+		for (MicroService m : microServicesQueue) {
+			try {
+				BlockingQueue<Message> messageQueue = ServicesMessageQueues.get(m);
+				if (messageQueue != null) {
+					messageQueue.put(b);
+				}
+			}
+			catch (InterruptedException ex) {
+				Thread.currentThread().interrupt();
+			}
+		}
+
+	}
+
+	
+	@Override
+	public <T> Future<T> sendEvent(Event<T> e) {
+		if(!EventsSubscribersQueues.containsKey(e.getClass())) {
+			System.out.println("No Such Event is listed please list it with a subscribing microservice first!");
+			return null;
+		}
+
+		if(EventsSubscribersQueues.get(e.getClass()).isEmpty()) {
+			System.out.println("No microservices subscribed with this event!");
+			return null;
+		}
+
+		MicroService m = EventsSubscribersQueues.get(e.getClass()).poll();
+		try {
+			ServicesMessageQueues.get(m).put(e);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+
+		try {
+			EventsSubscribersQueues.get(e.getClass()).put(m);
+		} catch (InterruptedException ex) {
+			Thread.currentThread().interrupt();
+		}
+
+		Future<T> output = new Future<>();
+		EventsFutures.put(e, output);
+		return output;
+	}
+
+	@Override
+	public void register(MicroService m) {
+
+		ServicesMessageQueues.putIfAbsent( m, new LinkedBlockingQueue<>());
+	}
+
+	@Override
+	public void unregister(MicroService m) {
+		if (!ServicesMessageQueues.containsKey(m))
+			return;
+
+		BlockingQueue<Message> queue = ServicesMessageQueues.get(m);
+		List<Event<?>> eventsToRemove = new ArrayList<>();
+
+		for (Message message : queue) {
+			if (message instanceof Event) {
+				Event<?> event = (Event<?>) message;
+				eventsToRemove.add(event);
+				EventsFutures.get(event).resolve(null);
+
+			}
+		}
+
+		for (Event<?> event : eventsToRemove) {
+			EventsFutures.remove(event);
+		}
+
+		// הסרת המיקרו-שירות מכל התורים של Broadcast
+		for (BlockingQueue<MicroService> broadcastQueue : BrodcastSubscribersQueues.values()) {
+			broadcastQueue.remove(m);
+		}
+
+		// הסרת המיקרו-שירות מכל התורים של Events
+		for (BlockingQueue<MicroService> eventQueue : EventsSubscribersQueues.values()) {
+			eventQueue.remove(m);
+		}
+
+		ServicesMessageQueues.remove(m);
+
+	}
+
+	@Override
+	public Message awaitMessage(MicroService m) throws InterruptedException {
+		return ServicesMessageQueues.get(m).take();
+	}
+
+}
