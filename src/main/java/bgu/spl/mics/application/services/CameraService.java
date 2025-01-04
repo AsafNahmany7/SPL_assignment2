ca60db9f0d8929b7306848f5093cc592a2a6b094
ShohAmar <shohamar5@gmail.com>
2025-01-04 23:49:13 +0200

    Messages,2services

--- src/main/java/bgu/spl/mics/application/services/CameraService.java
+++ src/main/java/bgu/spl/mics/application/services/CameraService.java
@@ -41,9 +41,11 @@ public class CameraService extends MicroService {
         // הרשמה לטיק
         subscribeBroadcast(TickBroadcast.class, tick -> {
             if (camera.getStatus() == Camera.status.UP) {
-                StampedDetectedObjects detectedObjects = camera.detectObjectsAtTime(tick.getCurrentTick());
-                if (detectedObjects != null && !detectedObjects.getDetectedObjects().isEmpty()) {
-                    sendEvent(new DetectObjectsEvent(detectedObjects));
+                if (camera.getFrequency() < tick.getCurrentTick()) {
+                    StampedDetectedObjects detectedObjects = camera.detectObjectsAtTime(tick.getCurrentTick() - camera.getFrequency());
+                    if (detectedObjects != null && !detectedObjects.getDetectedObjects().isEmpty()) {
+                        sendEvent(new DetectObjectsEvent(detectedObjects));
+                    }
                 }
             }
         });
